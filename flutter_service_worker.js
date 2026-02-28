6a09993ea9641380539407c46985c83ca18442b7
sdrarunvarshan <sdrarunvarshan@gmail.com>
2026-02-28 12:17:29 +0530

    Update project files and assets

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -159,7 +159,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "29e1498f9273139e1b4712c63bedde7e
 "canvaskit/skwasm_heavy.js.symbols": "0755b4fb399918388d71b59ad390b055",
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "9081284286a4ffbd1b2eb64e1f403cc5",
+"flutter_bootstrap.js": "f63436318277e6ffdf62978c7cf87578",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
