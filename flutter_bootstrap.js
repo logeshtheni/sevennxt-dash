e7d92a4a7b99d3bbe30b2ed702b83d147c9682ed
sdrarunvarshan <sdrarunvarshan@gmail.com>
2026-03-02 23:53:30 +0530

    Updated Flutter web build files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3110600279"
+    serviceWorkerVersion: "1252665122"
   }
 });
